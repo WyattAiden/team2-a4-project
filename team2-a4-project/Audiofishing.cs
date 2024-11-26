f6abca2df6b3928f63a6f5de6f4ac803b7f37b71
WyattAiden <wyatt.sobota@mohawkcollege.ca>
2024-11-26 14:51:28 -0500

    add music to game

--- team2-a4-project/Audiofishing.cs
+++ team2-a4-project/Audiofishing.cs
@@ -8,11 +8,13 @@ namespace Game10003
         //in your public class put Audiofishing af = new Audiofishing();
         Sound Splash;
         Sound Cast;
+        Music mucast;
         public void Setup()
         {
             //Loading the Audio
             Splash = Audio.LoadSound("Assets/water-splash-80537.mp3");
             Cast = Audio.LoadSound("Assets/Fishing_rod_cast.ogg");
+            mucast =Audio.LoadMusic("Assets/StockTune-Angler's Anthem_1732650117.mp3");
             Audio.SetPan(Cast, 1);
             Audio.SetPan(Splash, 0);
         }
@@ -25,6 +27,10 @@ namespace Game10003
         public void cast()
         {
             Audio.Play(Cast);
+        }
+        public void music()
+        {
+            Audio.Play(mucast);
         }
 
     }
