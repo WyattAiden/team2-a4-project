861c8fcfd1f9c054c2cf7d2af23d7150c52d7d38
WyattAiden <wyatt.sobota@mohawkcollege.ca>
2024-12-02 17:42:11 -0500

    Create Array for Textures and Path Directory to Assets. Johnson had technical difficulties commiting to his own branch

---
+++ team2-a4-project/Fish.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Game10003;
+
+namespace Group2_A4_Fishing
+{
+    public class Fish
+    {
+        //Place Your Variables Here
+        public int number;
+
+        public void Setup()
+        {
+            //Set Array for Paths to Images
+            string[] fishArray = { "Assets/Clam.png", "Assets/Crab.png", "Assets/Marlin.png", "Assets/Salmon.png", "Assets/Seahorse.png", "Assets/Shark.png", "Assets/Shrimp.png", "Assets/Siamese_Fighting_Fish.png", "Assets/SunFish.png", "Assets/Wahoo.png" };
+
+            //Set Array for Amount of Textures in the Texture Array
+            Texture2D[] texture = new Texture2D[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                texture[i] = Graphics.LoadTexture(fishArray[1]);
+            }
+        }
+        public void Update()
+        {
+            //Randomize Variable Number
+            number = Random.Integer(0, 9);
+        }
+    }
+}
